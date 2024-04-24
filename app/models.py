1fd821720e9f80fb5f68f8a57f1625af7bab16a3
PMIChampion <vdburdinskij@mai.education>
2024-04-24 22:10:00 +0300

    Основная логика

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from app import db
 from datetime import datetime
 from flask_login import UserMixin
 from app import login
+import random
 
 
 class User(UserMixin, db.Model):
@@ -13,6 +14,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(128), unique=True, index=True, nullable=False)
     password_hashed = db.Column(db.String(200), nullable=False)
     orders = db.relationship('Order', backref='user', lazy='dynamic')
+    role = db.Column(db.String(20), default='user')
 
     def set_password(self, password):
         self.password_hashed = generate_password_hash(password)
@@ -31,11 +33,43 @@ class Order(db.Model):
     price = db.Column(db.String(140), nullable=False)
     order_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    driver_id = db.Column(db.Integer, db.ForeignKey('driver.id'), nullable=True)
 
     def __repr__(self):
         return '<Order: start - {}, finish - {}, price - {}, time - {}>'.format(self.place_start, self.place_end, self.price, self.order_time)
 
+    def set_price(self, start, end):
+        self.price = random.randint(1, 200)
+
+    def set_order_time(self):
+        self.order_time = datetime.now()
+
 
 @login.user_loader
 def load_user(id):
+    driver = Driver.query.get(int(id))
+    if driver:
+        return driver
     return User.query.get(int(id))
+
+
+class Driver(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(64), index=True, nullable=False)
+    last_name = db.Column(db.String(64), index=True, nullable=False)
+    middle_name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(128), unique=True, index=True, nullable=False)
+    car_model = db.Column(db.String(64), index=True, nullable=False)
+    license_plate = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    password_hashed = db.Column(db.String(200), nullable=False)
+    orders = db.relationship('Order', backref='driver', lazy='dynamic')
+    role = db.Column(db.String(20), default='driver')
+
+    def set_password(self, password):
+        self.password_hashed = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hashed, password)
+
+    def __repr__(self):
+        return '<Driver {}, {}, {}, {}, {}>'.format(self.first_name, self.last_name, self.middle_name, self.email, self.password_hashed)
--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,20 @@ from urllib import request
 
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, OrderForm, DriverRegistrationForm
 from flask_login import login_user, logout_user, current_user, login_required
-from app.models import User
+from app.models import User, Order, Driver
 from werkzeug.urls import url_parse
+from app.utils import login_required
 
 
 @app.route('/')
+def choice():
+    return render_template('choice.html')
+
+
 @app.route('/index')
-@login_required
+@login_required(('user',))
 def index():
     posts = [
         {
@@ -35,6 +40,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid email or password!')
             return redirect(url_for('login'))
+        logout_user()
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
@@ -65,5 +71,55 @@ def register():
 
 
 @app.route('/order', methods=['GET', 'POST'])
-@login_required
+@login_required(('user',))
 def order():
+    form = OrderForm()
+    if form.validate_on_submit():
+        order = Order(place_start=form.place_start.data, place_end=form.place_end.data, user_id=current_user.id)
+        order.set_order_time()
+        order.set_price(form.place_start.data, form.place_end.data)
+        db.session.add(order)
+        db.session.commit()
+        flash('Thanks for your order, Taxi is on the way!')
+        return redirect(url_for('index'))
+    return render_template('order.html', title='Order', form=form)
+
+
+@app.route('/driver_register', methods=['GET', 'POST'])
+def driver_register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = DriverRegistrationForm()
+    if form.validate_on_submit():
+        driver = Driver(first_name=form.first_name.data, last_name=form.last_name.data, middle_name=form.middle_name.data, car_model=form.car_model.data, license_plate=form.license_plate.data, email=form.email.data)
+        driver.set_password(form.password.data)
+        db.session.add(driver)
+        db.session.commit()
+        flash('Thanks for registering!')
+        return redirect(url_for('login'))
+    return render_template('driver_register.html', title='Register', form=form)
+
+
+@app.route('/driver_login', methods=['GET', 'POST'])
+def driver_login():
+    if current_user.is_authenticated:
+        return redirect("driver_main")
+    form = LoginForm()
+    if form.validate_on_submit():
+        driver = Driver.query.filter_by(email=form.email.data).first()
+        if driver is None or not driver.check_password(form.password.data):
+            flash('Invalid email or password!')
+            return redirect(url_for('driver_login'))
+        logout_user()
+        login_user(driver, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('driver_main')
+        return redirect(next_page)
+    return render_template('driver_login.html', title='Sign In', form=form)
+
+
+@app.route('/driver_main')
+@login_required('driver')
+def driver_main():
+    return render_template('driver_main.html', title='Home')
